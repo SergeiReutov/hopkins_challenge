382fbc3da781774ae57ace1ad2a616de901d3dd9
Sergei Reutov <basic.mehmat@gmail.com>
2020-09-07 08:55:48 +0200

    Wind direction shortened

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -4,13 +4,13 @@ import { lessThan, greaterThanOrEqual } from 'utils/common';
 export const getTemperatureLabel = temp => `${Math.round(temp)} °C`;
 
 export const getDirectionByDegree = R.cond([
-  [lessThan(22.5), R.always('North')],
-  [lessThan(67.5), R.always('North - East')],
-  [lessThan(112.5), R.always('East')],
-  [lessThan(157.5), R.always('South - East')],
-  [lessThan(202.5), R.always('South')],
-  [lessThan(247.5), R.always('South - West')],
-  [lessThan(292.5), R.always('West')],
-  [lessThan(337.5), R.always('North - West')],
-  [greaterThanOrEqual(337.5), R.always('North')]
+  [lessThan(22.5), R.always('N')],
+  [lessThan(67.5), R.always('NE')],
+  [lessThan(112.5), R.always('E')],
+  [lessThan(157.5), R.always('SE')],
+  [lessThan(202.5), R.always('S')],
+  [lessThan(247.5), R.always('SW')],
+  [lessThan(292.5), R.always('W')],
+  [lessThan(337.5), R.always('NW')],
+  [greaterThanOrEqual(337.5), R.always('N')]
 ]);
